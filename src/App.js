4919d6dc1a93d6eb7dab1933755d3cb40781db80
Justin H <io@justinh.org>
2018-01-10 23:43:25 -0800

    basic non-component version complete

--- src/App.js
+++ src/App.js
@@ -2,18 +2,48 @@
 
 import React, { Component } from 'react'
 import logo from './logo.svg'
+import spinner from './spinner.svg'
 import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loading: true,
+      user: null,
+      followers: [],
+    }
+  }
+
+  componentDidMount() {
+    const url = 'https://api.github.com/users/prattsj'
+
+    fetch(url)
+      .then(response => response.json())
+      .then((user) => {
+        this.setState({ user, loading: false })
+      })
+  }
+
   render() {
+    const { user } = this.state
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {
+            user ? (
+              <div>
+                <h1>GitHub User: {user.login} </h1>
+                <h2>Followers: {user.followers} </h2>
+              </div>
+            ) : (
+              <img src={spinner} className="App-logo" alt="Loading..." height="120px" />
+            )
+          }
         </p>
       </div>
     )
